f7c69648f9e2de2854f0a3968e99cb403c8da049
phongnq <phongnq2712@gmail.com>
2022-02-23 19:21:42 -0500

    init Project

---
+++ Group9_FinalProject/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.group9.finalproject',
+  appName: 'Group9_FinalProject',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
